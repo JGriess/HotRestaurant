cf8a0644e71fa2d8a3d4357a2cf257e7f9511c51
John Kim <johnk9000@gmail.com>
2020-07-15 14:44:19 -0500

    Got an MVP working - needs styling

--- server.js
+++ server.js
@@ -1,28 +1,56 @@
 // Dependencies
 // =============================================================
-const express = require('express');
-const path = require('path');
+var express = require("express");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT|| 8080;
+var PORT = 3000;
 
-// DATA - reservations
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Reservations and waitlist (DATA)
 // =============================================================
-var reservations = [{
-
-}];
+var reservations = [];
+var waitlist = [];
 
 // Routes
 // =============================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "main.html"));
+});
 
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-// Render Reservation List
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// retrieve reservations
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+});
+
+// retrieve waitlist
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+app.post("/api/reservations", function(req, res) {
+    var newReservation = req.body;
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newReservation);
+    reservations.push(newReservation);
+    res.json(newReservation);
+})
+// Starts the server to begin listening
 // =============================================================
-
-
-// INIT ========================================================
-app.listen(PORT, () => {
-
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
